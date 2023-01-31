0cd7049f9e18a65ce76d6e93fc89ad3ffacbc7d4
admin <admin@dk-phone.local>
2023-01-31 10:19:56 +0900

    add config

--- pcconfig.py
+++ pcconfig.py
@@ -4,5 +4,6 @@ import pynecone as pc
 config = pc.Config(
     app_name="home",
     db_url="sqlite:///pynecone.db",
-    env=pc.Env.PROD
+    env=pc.Env.PROD,
+    api_url="ws://35.212.187.170:8000",
 )

7ea3971203d69a66931005bca8335f0c33f008b9
김동영 <32670163+DongyoungKim2@users.noreply.github.com>
2023-03-10 17:39:21 +0900

    modify backend api route

--- pcconfig.py
+++ pcconfig.py
@@ -5,6 +5,6 @@ config = pc.Config(
     app_name="home",
     db_url="sqlite:///pynecone.db",
     env=pc.Env.PROD,
-    api_url="ws://dongyoungkim.net:8000",
+    api_url="ws://dykim.dev:8000",
     port="80",
 )

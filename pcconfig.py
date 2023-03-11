3f8b9ba0ec618e258d174637ad47e4a9934b6bcb
김동영 <32670163+DongyoungKim2@users.noreply.github.com>
2023-03-11 23:36:19 +0900

    config

--- pcconfig.py
+++ pcconfig.py
@@ -6,7 +6,5 @@ config = pc.Config(
     db_url="sqlite:///pynecone.db",
     env=pc.Env.PROD,
     api_url="ws://35.212.187.170:8000",
-    port="80",
-    backend_port="8000",
-    deploy_url="https://dykim.dev"
+    port="80"
 )

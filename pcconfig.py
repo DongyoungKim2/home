271a5a9c2928b982845f50bc7e0f48038690674f
김동영 <32670163+DongyoungKim2@users.noreply.github.com>
2023-03-11 13:56:32 +0900

    api update

--- pcconfig.py
+++ pcconfig.py
@@ -5,6 +5,6 @@ config = pc.Config(
     app_name="home",
     db_url="sqlite:///pynecone.db",
     env=pc.Env.PROD,
-    api_url="http://35.212.187.170:8000/",
+    api_url="http://35.212.187.170:8000",
     port="80",
 )

f0d1887d4418392fdfae3347c269ca862c544dcc
김동영 <32670163+DongyoungKim2@users.noreply.github.com>
2023-03-13 21:53:44 +0900

    update pynecone

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,8 @@ FROM dongyoungkim/home:0.1.7
 RUN rm -rf ./home 
 RUN git clone https://github.com/DongyoungKim2/home.git ./home
 
+RUN pip install --upgrade pynecone
+
 WORKDIR /app/home
 
 RUN pc init
--- pcconfig.py
+++ pcconfig.py
@@ -5,7 +5,6 @@ config = pc.Config(
     app_name="home",
     db_url="sqlite:///pynecone.db",
     env=pc.Env.PROD,
-    bun_path="$HOME/.bun/bin/bun",
-    api_url="ws://35.212.232.153:8000",
+    api_url="ws://35.212.187.170:8000",
     port="80"
 )
